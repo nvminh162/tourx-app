6764c8276f0789f6cda7e3bdb977d7a903db23bf
nvminh162 <nvminh162@gmail.com>
2025-02-08 02:42:10 +0700

    add Tailwind CSS integration and update project metadata

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
-import clsx from "clsx";
-import styles from './Home.module.scss'
+import clsx from 'clsx';
+import styles from './Home.module.scss';
 
 function Home() {
-    const classes = clsx(styles.wrapper)
+    const classes = clsx(styles.wrapper);
 
     return (
         <div className={classes}>
-            Home page
+            <h1 class="text-5xl font-bold flex justify-center text-red-800">Home page</h1>
         </div>
     );
 }
