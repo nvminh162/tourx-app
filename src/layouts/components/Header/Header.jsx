d4865abcb1b735c585a41982b67df2a71803d5a6
nvminh162 <nvminh162@gmail.com>
2025-02-08 15:57:16 +0700

    add header buttons and update styles with CSS variables

--- src/layouts/components/Header/Header.jsx
+++ src/layouts/components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import clsx from 'clsx';
+
 import styles from './Header.module.scss';
+import Button from '~/components/Button';
+import config from '~/config';
 
 function Header() {
     const classes = clsx('header', styles.wrapper);
 
-    return <div className={classes}>Header layout</div>;
+    return (
+        <div className={classes}>
+            <span>Header layout</span>
+            <div>
+                <Button primary to={config.routes.home}>Home</Button>
+                <Button to={config.routes.login}>Login</Button>
+                <Button outline to={config.routes.search}>Search</Button>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
