d1d5809b8244f87ed4ec03360cb0409bfc5c3418
nvminh162 <nvminh162@gmail.com>
2025-02-25 18:01:44 +0700

    refactor: update Vite config to use path resolution with fileURLToPath and dirname

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,37 @@
-/* eslint-disable no-undef */
+// /* eslint-disable no-undef */
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+// import path from 'path';
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//     plugins: [react(), tailwindcss()],
+//     resolve: {
+//         alias: {
+//             '~': path.resolve(process.cwd(), 'src'),
+//         },
+//     },
+//     server: {
+//         port: 9999,
+//     },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
-            '~': path.resolve(process.cwd(), 'src'),
+            '~': resolve(__dirname, 'src'),
         },
     },
     server: {
