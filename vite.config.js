6f9a547e539aec303cec2aebdcefc3075c884843
nvminh162 <nvminh162@gmail.com>
2025-02-25 18:56:48 +0700

    refactor: remove image asset inclusion from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
     server: {
         port: 9999,
     },
